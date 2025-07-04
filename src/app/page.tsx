e790665ad80bf1ba5a00acf43363a839a1e24f12
minsu <a2238737@gmail.com>
2025-07-04 23:22:39 +0900

    [feat & Design] : storybook AppRouter 추가 및 디자인 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import Header from '@/components/header/Header';
-
 export default function Home() {
   return (
     <div>
       <main>
-        <Header pageType="home" festivalName="축제이름" isLogin={true} />
         <h1>Hello World</h1>
       </main>
     </div>
