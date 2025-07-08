b3bc7907b77c5b18bccef0332d7247e67c5344f9
juyeongeun <80696592+juyeongeun@users.noreply.github.com>
2025-07-08 22:36:39 +0900

    [feat]: create map

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
+'use client';
+
+import Map from './_components/Map';
+import styles from './page.module.css';
+
+// 축제 데이터 상수 정의
+const FESTIVAL_DATA = {
+  ACTIVE: [
+    { festivalRegion: '0' }, // 거제시
+    { festivalRegion: '1' }, // 거창군
+    { festivalRegion: '2' }, // 고성군
+  ],
+  SOON: [
+    { festivalRegion: '3' }, // 김해시
+  ],
+};
+
 export default function Home() {
   return (
-    <div>
-      <main>
-        <h1>Hello World</h1>
-      </main>
-    </div>
+    <>
+      <div className={styles.banner_container}>
+        <p>배너</p>
+      </div>
+      <Map
+        isActiveFestival={FESTIVAL_DATA.ACTIVE}
+        soonActiveFestival={FESTIVAL_DATA.SOON}
+      />
+    </>
   );
 }
