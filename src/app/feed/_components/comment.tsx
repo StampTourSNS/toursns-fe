d2f8a6566d7f938652c59d1099f824535aaa2d6b
minsu <a2238737@gmail.com>
2025-07-10 15:18:04 +0900

    [Refactor] : CSS 수정

--- src/app/feed/_components/comment.tsx
+++ src/app/feed/_components/comment.tsx
@@ -2,23 +2,21 @@
 
 import { useEffect, useRef, useState } from 'react';
 
-import Image from 'next/image';
+import { X } from 'lucide-react';
 
-import { MoreHorizontal, User, X } from 'lucide-react';
+import CommentUser from '@/components/commentUser/CommentUser';
 
 import mockData from '../mockData.json';
 
 import styles from './comment.module.css';
 
-export default function Comment({
-  show,
-  onClose,
-  feed,
-}: {
+interface CommentProps {
   show: boolean;
   onClose: () => void;
   feed: (typeof mockData.feed)[0];
-}) {
+}
+
+export default function Comment({ show, onClose, feed }: CommentProps) {
   const [openDropdownId, setOpenDropdownId] = useState<number | null>(null);
   const dropdownRef = useRef<HTMLDivElement>(null);
 
@@ -45,53 +43,13 @@ export default function Comment({
         <X />
       </button>
       {feed.comments.map((comment) => (
-        <div className={styles.comment_content} key={comment.id}>
-          {comment.user.image !== 'null' ? (
-            <Image
-              src={comment.user.image}
-              width={38}
-              height={38}
-              alt={'profileImage'}
-              className={styles.comment_user_profile_image}
-            />
-          ) : (
-            <User
-              className={styles.comment_user_profile_image}
-              size={38}
-              strokeWidth={1.5}
-            />
-          )}
-          <div className={styles.comment_user_info}>
-            <p className={styles.comment_user_name}>{comment.user.name}</p>
-            <p>{comment.content}</p>
-          </div>
-          <div className={styles.comment_user_menu_container}>
-            <div
-              className={styles.comment_user_menu_icon_container}
-              onClick={() =>
-                setOpenDropdownId(
-                  openDropdownId === comment.id ? null : comment.id,
-                )
-              }
-            >
-              <MoreHorizontal
-                size={16}
-                strokeWidth={1.5}
-                className={styles.comment_user_menu_icon}
-              />
-            </div>
-            {openDropdownId === comment.id && (
-              <div className={styles.comment_dropdown_menu} ref={dropdownRef}>
-                <button className={styles.comment_dropdown_menu_button}>
-                  수정하기
-                </button>
-                <button className={styles.comment_dropdown_menu_button}>
-                  삭제하기
-                </button>
-              </div>
-            )}
-          </div>
-        </div>
+        <CommentUser
+          key={comment.id}
+          imageUrl={comment.user?.image}
+          name={comment.user?.name}
+          comment={comment.content}
+          isUser={true}
+        />
       ))}
     </div>
   );
