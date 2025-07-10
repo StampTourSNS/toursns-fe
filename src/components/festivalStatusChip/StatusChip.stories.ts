917c611f6756a824abff4b05f1a5da1ed3c09251
천세윤 <tpdbs0701@gmail.com>
2025-07-10 15:54:03 +0900

    feat : 축제 상태 칩 생성

---
+++ src/components/festivalStatusChip/StatusChip.stories.ts
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Meta, StoryObj } from '@storybook/react';
+import StatusChip, { StatusType } from './StatusChip';
+
+const meta: Meta<typeof StatusChip> = {
+  title: 'Common/StatusChip',
+  component: StatusChip,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof StatusChip>;
+
+export const Ongoing: Story = {
+  args: {
+    status: 'Ongoing',
+  },
+};
+
+export const Scheduled: Story = {
+  args: {
+    status: 'Scheduled',
+  },
+};
+
+export const End: Story = {
+  args: {
+    status: 'End',
+  },
+};
